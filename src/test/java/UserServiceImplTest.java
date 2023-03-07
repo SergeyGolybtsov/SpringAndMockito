0db7b0064e81dd9a5fe5ff0b503d101823b43723
SergeyGolybtsov <kirsa200@gmail.com>
2023-03-07 19:17:57 +0300

    Выполнил ДЗ (Доработал 2)

--- src/test/java/UserServiceImplTest.java
+++ src/test/java/UserServiceImplTest.java
@@ -8,11 +8,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
     private static final User VALID_USER = new User("Сергей");
     private static final User INVALID_USER = new User("Жук");
+    private static final List<User> user = List.of(VALID_USER, new User("Андрей"));
 
     @Mock
     UserRepository userRepository;
@@ -22,12 +25,16 @@ public class UserServiceImplTest {
     @Test
     @DisplayName("Возвращает true если пользователь с именем есть в списке")
     void shouldReturnTrue() {
+        when(userRepository.getAllUser()).thenReturn(user);
+        Assertions.assertEquals(user, userRepository.getAllUser());
         Assertions.assertTrue(userService.checkUserExist(VALID_USER));
     }
 
     @Test
     @DisplayName("Возвращает false если пользователя с именем нет в списке")
     void shouldReturnFalse() {
+        when(userRepository.getAllUser()).thenReturn(user);
+        Assertions.assertEquals(user, userRepository.getAllUser());
         Assertions.assertFalse(userService.checkUserExist(INVALID_USER));
     }
 }
